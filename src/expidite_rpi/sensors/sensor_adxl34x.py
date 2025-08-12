9d7332f42120b0a411cdabf4d033f1deaefc5b9e
stuart-bee-ops <stuart@bee-ops.com>
2025-08-12 15:09:27 +0100

    Create sensor_adxl34x.py

---
+++ src/expidite_rpi/sensors/sensor_adxl34x.py
@@ -0,0 +1,114 @@
+##########################################################################################################
+# RpiCore wrapper for ADXL34X
+##########################################################################################################
+import time
+from dataclasses import dataclass
+
+import board
+import pandas as pd
+from expidite_rpi.core import api
+from expidite_rpi.core import configuration as root_cfg
+from expidite_rpi.core.dp_config_objects import Stream
+from expidite_rpi.core.sensor import Sensor, SensorCfg
+from expidite_rpi.sensors.drivers import adafruit_adxl34x
+
+logger = root_cfg.setup_logger("expidite")
+
+ADXL34X_STREAM_INDEX = 0
+ADXL34X_SENSOR_INDEX = 83 # ADXL34X i2c address, 0x53 (83)
+ADXL34X_SENSOR_TYPE_ID = "ADXL34X"
+ADXL34X_FIELDS = ["RMS_X", "RMS_Y", "RMS_Z", "RMS_MAGNITUDE",
+                  "PEAK_X", "PEAK_Y", "PEAK_Z", "PEAK_MAGNITUDE",
+                  "STD_DEV_X", "STD_DEV_Y", "STD_DEV_Z", "STD_DEV_MAGNITUDE"]
+
+@dataclass
+class ADXL34XSensorCfg(SensorCfg):
+    ############################################################
+    # SensorCfg fields
+    ############################################################
+    # The type of sensor.
+    sensor_type: api.SENSOR_TYPE = api.SENSOR_TYPE.I2C
+    sensor_index: int = ADXL34X_SENSOR_INDEX
+    sensor_model: str = "ADXL34X"
+    # A human-readable description of the sensor model.
+    description: str = "ADXL34X accelerometer"
+
+    ############################################################
+    # Custom fields
+    ############################################################
+
+DEFAULT_ADXL34X_SENSOR_CFG = ADXL34XSensorCfg(
+    outputs=[
+        Stream(
+            description="Acceleration data from ADXL34X",
+            type_id=ADXL34X_SENSOR_TYPE_ID,
+            index=ADXL34X_STREAM_INDEX,
+            format=api.FORMAT.LOG,
+            fields=ADXL34X_FIELDS,
+            cloud_container="expidite-journals",
+        )
+    ],
+)
+
+class ADXL34X(Sensor):
+    # Init
+    def __init__(self, config: ADXL34XSensorCfg):
+        super().__init__(config)
+        self.config = config
+
+    def run(self):
+        accelerometer = None
+        while self.continue_recording():
+            try:
+                if accelerometer is None:
+                    i2c = board.I2C()  # uses board.SCL and board.SDA
+                    accelerometer = adafruit_adxl34x.ADXL343(i2c)  # type: ignore
+                    accelerometer.set_data_rate(adafruit_adxl34x.DataRate.RATE_400_HZ)
+                    accelerometer.set_range(adafruit_adxl34x.Range.RANGE_4_G)
+
+                # Capture approximately 1 second of X, Y, and Z instantaneous accelerations readings
+                start_time = api.utc_now()
+                xyz_df = self.capture_data_block(accelerometer)
+                xyz_df['magnitude'] = (xyz_df['aX']**2 + xyz_df['aY']**2 + xyz_df['aZ']**2) ** 0.5
+
+                # Calculate the RMS from the data_df for each of X, Y, Z and the overall vector
+                rms = (xyz_df ** 2).mean() ** 0.5
+
+                # Calculate peak acceleration for each of X, Y, Z and overall vector
+                peak = xyz_df.max()
+
+                # Calculate the standard deviation for each of X, Y, Z and overall vector
+                std_dev = xyz_df.std()
+
+                self.log(
+                    stream_index=ADXL34X_STREAM_INDEX,
+                    sensor_data={
+                        "timestamp": start_time,
+                        "RMS_X": ("%.1f" % rms['aX']),
+                        "RMS_Y": ("%.1f" % rms['aY']),
+                        "RMS_Z": ("%.1f" % rms['aZ']),
+                        "RMS_MAGNITUDE": ("%.1f" % rms['magnitude']),
+                        "PEAK_X": ("%.1f" % peak['aX']),
+                        "PEAK_Y": ("%.1f" % peak['aY']),
+                        "PEAK_Z": ("%.1f" % peak['aZ']),
+                        "PEAK_MAGNITUDE": ("%.1f" % peak['magnitude']),
+                        "STD_DEV_X": ("%.1f" % std_dev['aX']),
+                        "STD_DEV_Y": ("%.1f" % std_dev['aY']),
+                        "STD_DEV_Z": ("%.1f" % std_dev['aZ']),
+                        "STD_DEV_MAGNITUDE": ("%.1f" % std_dev['magnitude']),
+                    },
+                )
+
+            except Exception as e:
+                logger.error(f"{root_cfg.RAISE_WARN()}Error in ADXL34X sensor run: {e}", exc_info=True)
+
+    def capture_data_block(self, accelerometer: adafruit_adxl34x.ADXL343) -> pd.DataFrame:
+        """ Capture approximately 1 second of readings"""
+        data = []
+        start_time = time.monotonic()
+        while time.monotonic() - start_time < 1:
+            data.append(accelerometer.acceleration)
+
+        # Convert the list of tuples [int, int, int] to a DataFrame
+        df = pd.DataFrame(data, columns=["aX", "aY", "aZ"])
+        return df
