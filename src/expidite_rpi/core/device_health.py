6e4192888e28d9bf6870a46530f30ab10adf73a0
Nick Butcher <NickButcher1@users.noreply.github.com>
2025-11-14 13:45:15 +0000

    Reduce memory use threshold for reboot to 90%
    90% is generally considered good practice for reboot. At 95%, performance is more likely already degraded.

--- src/expidite_rpi/core/device_health.py
+++ src/expidite_rpi/core/device_health.py
@@ -309,8 +309,11 @@ class DeviceHealth(Sensor):
             if memory_usage > 75:
                 if root_cfg.running_on_rpi:
                     DeviceHealth.log_top_memory_processes()
-                    if memory_usage > 95:
-                        logger.error(root_cfg.RAISE_WARN() + "Memory usage >95%, rebooting")
+                    # Running low on free RAM can cause any OS process to be killed to free up memory, and can
+                    # cause performance degradation. Triggering a controlled reboot at 90% memory usage is
+                    # generally considered good practice to recover before performance degrades.
+                    if memory_usage > 90:
+                        logger.error(root_cfg.RAISE_WARN() + "Memory usage >90%, rebooting")
                         utils.run_cmd("sudo reboot", ignore_errors=True)
 
             # Get the expidite version and user code version from the files
