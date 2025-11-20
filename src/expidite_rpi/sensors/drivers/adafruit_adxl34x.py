8ca6c87ca5bba72aeb477d20be111d9798ab172d
Nick Butcher <NickButcher1@users.noreply.github.com>
2025-11-20 16:17:58 +0000

    Enable lint flake8-pie and fix violations

--- src/expidite_rpi/sensors/drivers/adafruit_adxl34x.py
+++ src/expidite_rpi/sensors/drivers/adafruit_adxl34x.py
@@ -41,7 +41,6 @@ try:
 except (ImportError, NotImplementedError):
     # Running on non-CircuitPython environment (Windows/standard Python)
     board = None
-    pass
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ADXL34x.git"
