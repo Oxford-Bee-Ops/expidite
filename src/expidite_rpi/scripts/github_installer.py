d8d912cd3f427c0f7043e955a2d2e65e4414770d
Nick Butcher <NickButcher1@users.noreply.github.com>
2026-01-15 16:09:42 +0000

    WIP

--- src/expidite_rpi/scripts/github_installer.py
+++ src/expidite_rpi/scripts/github_installer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import subprocess
+import sys
+import tempfile
 from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+from typing import Any
 
 from github import Auth, Github, GithubException
 from packaging.version import Version
@@ -44,8 +49,7 @@ def _get_installed_bee_ops_version() -> str:
         return "0.0.0"
 
 
-def _get_latest_bee_ops_version() -> str:
-    g = Github(auth=Auth.Token(_get_my_github_pat()))
+def _get_latest_bee_ops_version(g: Github) -> tuple[str, Any | None]:
     repo = g.get_repo(_get_repo_path())
     releases = repo.get_releases()
     if releases is None or releases.totalCount == 0:
@@ -54,6 +58,7 @@ def _get_latest_bee_ops_version() -> str:
     # Only interested in releases for the configured branch.
     my_git_branch = _get_my_git_banch()
     latest_version_found = "0.0.0"
+    latest_release_found = None
 
     for release in releases:
         release_branch = release.tag_name.split("-v")[0]
@@ -61,12 +66,32 @@ def _get_latest_bee_ops_version() -> str:
             release_version = release.tag_name.split("-v")[1]
             if Version(release_version) > Version(latest_version_found):
                 latest_version_found = release_version
+                latest_release_found = release
 
-    return latest_version_found
+    return latest_version_found, latest_release_found
 
 
-def _download_and_install_package(latest_version: str) -> None:
-    pass  # TODO
+def _download_and_install_package(release) -> None:
+    if release.assets is not None:
+        for asset in release.assets:
+            if asset.name.endswith(".whl"):
+                print(f"Downloading: {asset.name}")
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    local_wheel_path = Path(temp_dir) / asset.name
+                    asset.download_asset(str(local_wheel_path))
+                    _install_package(local_wheel_path)
+                    return
+
+
+def _install_package(local_wheel_path: Path) -> None:
+    try:
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "--upgrade", "--force-reinstall", str(local_wheel_path)]
+        )
+        print(f"Successfully installed {local_wheel_path.name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to install {local_wheel_path.name}: {e}")
+        raise
 
 
 def _install_bee_ops_package() -> None:
@@ -74,14 +99,16 @@ def _install_bee_ops_package() -> None:
     installed_version = _get_installed_bee_ops_version()
 
     try:
-        latest_version = _get_latest_bee_ops_version()
+        g = Github(auth=Auth.Token(_get_my_github_pat()))
+
+        latest_version, latest_release = _get_latest_bee_ops_version(g)
         print(f"bee_ops package: installed: {installed_version}, latest: {latest_version}")
 
         if installed_version == latest_version:
             print("Latest version already installed. No action needed.")
             return
 
-            _download_and_install_package(latest_version)
+        _download_and_install_package(latest_release)
     except GithubException as e:
         print(f"Failed to read bee_ops repo: {e}")
         raise
@@ -97,8 +124,6 @@ if __name__ == "__main__":
         print(f"Installation of bee_ops package failed: {e}")
         # TODO need a return code instead.
 
-# TODO Log and download the wheel.
-# TODO Log and pip install the wheel.
 # TODO MUST FAIL THE INSTALL OVERALL IF THIS STEP FAILS.
 # TODO If successful, log and set reboot_required (or defer to rpi_installer.sh).
 # TODO If successful, write new current_version file (or defer to rpi_installer.sh).
